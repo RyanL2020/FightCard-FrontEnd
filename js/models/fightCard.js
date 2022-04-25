65f31ead3e293491c37b5900cf4b4fa53dfbbaf9
RL2020 <loc.fitness1@gmail.com>
2022-04-24 21:50:28 -0400

    constructor FightCard

--- js/models/fightCard.js
+++ js/models/fightCard.js
@@ -1,4 +1,16 @@
-class fightCard {
+class FightCard {
 
     static all = []
+
+    constructor({id, fighter_id, comment_id}){
+
+        this.id = id,
+        this.fighter_id,
+        this.comment_id
+
+
+        FightCard.all.push(this)
+        
+
+    }
 }
